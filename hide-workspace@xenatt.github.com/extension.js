aee35ab4a3e9ec42dbfe461264eef09f340834e0
Nattapong Pullkhow <exenatt@gmail.com>
2014-04-24 14:41:59 +0700

    Update extension.js

--- hide-workspace@xenatt.github.com/extension.js
+++ hide-workspace@xenatt.github.com/extension.js
@@ -2,39 +2,26 @@ const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 const Main = imports.ui.main;
-const St = imports.gi.St;
-
-
-const hideVisible = new Lang.Class({
-    Name: 'hideWorkspace.hideVisible',
-    
-    _init: function() {
-        return true;
-    },
-
-    enable: function() {
-        this._hideThumbnails();
-    },
-
-    disable: function() {
-        this._showThumbnails();
-    },
-
-    _hideThumbnails: function() {
-
-        //Thank Api code from Charlie MacEntyre  passingthure67@gmail.com
-        Main.overview._controls._thumbnailsSlider.actor.opacity = 0;
-        return true;
-    },
-
-    _showThumbnails: function() {
-        Main.overview._controls._thumbnailsSlider.actor.opacity = 255;
-        return true;
-    }
-
-});
+const ThumbnailsSlider = imports.ui.overviewControls.ThumbnailsSlider.prototype; 
 
 function init() {
     return new hideVisible();
 }
 
+const hideVisible = new Lang.Class({
+    Name: 'hideWorkspace.hideVisible',
+    
+    _init: function() {
+        //old solution
+        //thumbnailsBoxOp = Main.overview._controls._thumbnailsSlider.actor.opacity;
+        tmp_getAlwaysZoomOut = ThumbnailsSlider._getAlwaysZoomOut;
+    },
+
+    enable: function() {
+        ThumbnailsSlider._getAlwaysZoomOut = function () { return false; }
+    },
+
+    disable: function() {
+        ThumbnailsSlider._getAlwaysZoomOut = tmp_getAlwaysZoomOut;
+    }
+});
