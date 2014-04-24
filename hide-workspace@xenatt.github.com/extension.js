c5f3bebcb8ebe45f2f30758c8bf5ec23d7b779eb
Nattapong Pullkhow <exenatt@gmail.com>
2014-04-24 15:06:45 +0700

    Update extension.js

--- hide-workspace@xenatt.github.com/extension.js
+++ hide-workspace@xenatt.github.com/extension.js
@@ -2,7 +2,7 @@ const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 const Main = imports.ui.main;
-const ThumbnailsSlider = imports.ui.overviewControls.ThumbnailsSlider.prototype; 
+const ThumbnailsSlider = imports.ui.overviewControls.ThumbnailsSlider.prototype;
 
 const init = function () {
     return new hideVisible();
@@ -15,14 +15,16 @@ const hideVisible = new Lang.Class({
         //old solution
         //thumbnailsBoxOp = Main.overview._controls._thumbnailsSlider.actor.opacity;
         tmp_getAlwaysZoomOut = ThumbnailsSlider._getAlwaysZoomOut;
+        tmp_getNonExpandedWidth = ThumbnailsSlider.getNonExpandedWidth;
     },
 
     enable: function() {
         ThumbnailsSlider._getAlwaysZoomOut = function () { return false; }
+        ThumbnailsSlider.getNonExpandedWidth = function () { return 0; }
     },
 
     disable: function() {
         ThumbnailsSlider._getAlwaysZoomOut = tmp_getAlwaysZoomOut;
+        ThumbnailsSlider.getNonExpandedWidth = tmp_getNonExpandedWidth;
     }
-
 });
